593a0f90a22544a3a3cb034e6cdeedf96e6da9b2
unknown <habiburrahmanofficial157@gmail.com>
2023-08-21 15:31:28 +0600

    code done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,77 @@
-function cardClickItem() {
-  console.log("object");
+let total = 0;
+let couponCode;
+
+function homeCookerItem(target) {
+  const selectedItem = document.getElementById("selected-items");
+  const itemName = target.childNodes[3].childNodes[3].innerText;
+  // console.log(itemName);
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  // console.log(li);
+  selectedItem.appendChild(li);
+  const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+  total = parseFloat(total) + parseFloat(price);
+  // console.log(total);
+  const firstTotal = (document.getElementById("total-price").innerText = total);
+  const priceTotal = firstTotal * 0.2;
+  const discountPrice = (document.getElementById("discount").innerText =
+    priceTotal);
+  const discount = firstTotal - priceTotal;
+  const totalPrice = (document.getElementById("total").innerText = discount);
 }
+
+
+function sportsCatesItem(target) {
+  const selectedItem = document.getElementById("sportsWear");
+  const itemName = target.childNodes[3].childNodes[3].innerText;
+  // console.log(itemName);
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  // console.log(li);
+  selectedItem.appendChild(li);
+  const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+  total = parseFloat(total) + parseFloat(price);
+  // console.log(total);
+  const firstTotal = (document.getElementById("total-price").innerText = total);
+  const priceTotal = firstTotal * 0.2;
+  const discountPrice = (document.getElementById("discount").innerText =
+    priceTotal);
+  const discount = firstTotal - priceTotal;
+  const totalPrice = (document.getElementById("total").innerText = discount);
+}
+
+
+function homeCookItem(target) {
+  const selectedItem = document.getElementById("furniture");
+  const itemName = target.childNodes[3].childNodes[3].innerText;
+  // console.log(itemName);
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  // console.log(li);
+  selectedItem.appendChild(li);
+  const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+  total = parseFloat(total) + parseFloat(price);
+  // console.log(total);
+  const firstTotal = (document.getElementById("total-price").innerText = total);
+  const priceTotal = firstTotal * 0.2;
+  const discountPrice = (document.getElementById("discount").innerText =
+    priceTotal);
+  const discount = firstTotal - priceTotal;
+  const totalPrice = (document.getElementById("total").innerText = discount);
+}
+
+
+
+
+const sellInput = document.getElementById("coupon-input");
+const sellButton = document.getElementById("coupon-button");
+sellInput.addEventListener("input", () => {
+  const inputText = sellInput.value.trim();
+  couponCode = inputText;
+  // console.log(inputText);
+  if (inputText === "SELL200") {
+    sellButton.removeAttribute("disabled");
+  } else {
+    sellButton.setAttribute("disabled", "true");
+  }
+});
